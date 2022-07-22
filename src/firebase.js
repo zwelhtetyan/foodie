3b829be31e739059755937bd5f7b1d2190ff536d
zwelhtetyan <zwelhtetyan2001@gmail.com>
2022-07-22 21:45:25 +0630

    using key with environment virable

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDMRxbGlcYCxVMiPuWJLjM5MxMGMcbG2s4',
-    authDomain: 'shopify-eed5f.firebaseapp.com',
-    databaseURL:
-        'https://shopify-eed5f-default-rtdb.asia-southeast1.firebasedatabase.app',
-    projectId: 'shopify-eed5f',
-    storageBucket: 'shopify-eed5f.appspot.com',
-    messagingSenderId: '1060239308734',
-    appId: '1:1060239308734:web:1ce37b214e431af4976bef',
+   //    apiKey: 'AIzaSyDMRxbGlcYCxVMiPuWJLjM5MxMGMcbG2s4',
+   apiKey: `${process.env.REACT_APP_API_KEY}`,
+   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+   databaseURL: `${process.env.REACT_APP_DB_URL}`,
+   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+   messagingSenderId: `${process.env.REACT_APP_MSGSENDER_ID}`,
+   appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
 // Initialize Firebase
